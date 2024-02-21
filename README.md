993a8fc316c7b5b1c0043a6417b70186eec06118
Hunter Parcells <hunterparcells@gmail.com>
2024-02-20 20:15:20 -0500

    feat: Initial bot.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # kick-bot-template
+> Template repository for creating a Kick.com bot with kient.
+
+## `.env`
+- `KICK_EMAIL`
+- `KICK_PASSWORD`
+- `KICK_2FA`
+
+### Optional
+- `KICK_AUTH`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Test.');
+import 'dotenv/config';
+
+import { Events, Kient } from 'kient';
+import OTP from 'otp';
+
+(async () => {
+  if (!process.env.KICK_EMAIL || !process.env.KICK_PASSWORD || !process.env.KICK_2FA) {
+    throw new Error('Missing authentication.');
+  }
+
+  const client = await Kient.create();
+  await client.api.authentication.login(
+    {
+      email: process.env.KICK_EMAIL,
+      password: process.env.KICK_PASSWORD,
+      otc: new OTP({
+        secret: process.env.KICK_2FA
+      }).totp(Date.now())
+    },
+    process.env.KICK_AUTH || undefined
+  );
+})();
